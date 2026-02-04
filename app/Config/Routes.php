1b0f70ab0d875fecb435527752b2092126523977
adimiuprix <miuprix@gmail.com>
2026-02-04 22:56:01 +0700

    admin assets di upload semua

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,5 +7,7 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/success', 'Home::success');
-$routes->post('telegram/send-message', 'Telegram::sendMessage');
-$routes->post('telegram/send-document', 'Telegram::sendDocument');
+$routes->post('/telegram/send-message', 'Telegram::sendMessage');
+$routes->post('/telegram/send-document', 'Telegram::sendDocument');
+
+$routes->get('/admin', 'Admin::index');
