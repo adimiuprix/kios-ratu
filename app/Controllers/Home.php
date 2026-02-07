04478af772fd6ef850bbb4e405762e4c5cc3f80b
adimiuprix <miuprix@gmail.com>
2026-02-07 08:49:47 +0700

    notif yang di home sudah di buat dinamis

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,7 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
 use App\Models\Setting;
+use App\Models\Notification;
 
 class Home extends BaseController
 {
@@ -11,25 +13,11 @@ class Home extends BaseController
         $setting = new Setting();
         $setting = $setting->first();
 
-        $data = [
-            'notifications' => [
-                ['name' => 'Andi Pratama', 'qty' => '2B', 'amount' => 126000],
-                ['name' => 'Siti Aminah', 'qty' => '500M', 'amount' => 31500],
-                ['name' => 'Budi Santoso', 'qty' => '1B', 'amount' => 63000],
-                ['name' => 'Dewi Lestari', 'qty' => '10B', 'amount' => 630000],
-                ['name' => 'Rizky Fauzi', 'qty' => '3B', 'amount' => 189000],
-                ['name' => 'Lutfi Hakim', 'qty' => '5B', 'amount' => 315000],
-                ['name' => 'Eka Wijaya', 'qty' => '700M', 'amount' => 44100],
-                ['name' => 'Sari Putri', 'qty' => '1.5B', 'amount' => 94500],
-                ['name' => 'Hendra Kurniawan', 'qty' => '12B', 'amount' => 756000],
-                ['name' => 'Maya Indah', 'qty' => '800M', 'amount' => 50400],
-                ['name' => 'Agus Setiawan', 'qty' => '4B', 'amount' => 252000],
-                ['name' => 'Rina Melati', 'qty' => '300M', 'amount' => 18900],
-                ['name' => 'Yudi Prayoga', 'qty' => '20B', 'amount' => 1260000],
-                ['name' => 'Anisa Fitri', 'qty' => '6B', 'amount' => 378000],
-                ['name' => 'Fajar Sidik', 'qty' => '2.5B', 'amount' => 157500],
-            ],
+        $notifications = new Notification();
+        $notifications = $notifications->findAll(15);
 
+        $data = [
+            'notifications' => $notifications,
             'title' => $setting['title'],
             'description' => $setting['description'],
             'keywords' => $setting['keywords'],
@@ -40,7 +28,6 @@ class Home extends BaseController
             'admin_img' => $setting['admin_img'],
             'admin_id' => $setting['admin_id'],
         ];
-
         return view('home', $data);
     }
 
